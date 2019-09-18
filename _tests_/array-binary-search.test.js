66eccc3a96b51df3bed810b149ac9b63bf2d636f
Abbey Masters <abbeykmasters@gmail.com>
2019-09-18 16:59:58 -0700

    test passing

---
+++ _tests_/array-binary-search.test.js
@@ -0,0 +1,10 @@
+const binarySearch = require('../challenges/arrayBinarySearch/array-binary-search.js');
+
+describe('using binary search function', () => {
+  it('using binary search function', () => {
+    expect(binarySearch([4,8,15,16,23,42], 15)).toEqual(2);
+  });
+  it('using binary search function #2', () => {
+    expect(binarySearch([11,22,33,44,55,66,77], 90)).toEqual(-1);
+    });
+  });
---
+++ challenges/arrayBinarySearch/README.md
@@ -0,0 +1,10 @@
+# Array Binary Search
+Today, I'm writing a function called BinarySearch which takes in 2 parameters: a sorted array and the search key and returns the index of the array’s element that is equal to the search key, or -1 if the element does not exist.
+
+## Challenge
+I can't utilize any of the built-in methods available in JavaScript.
+
+## Approach & Efficiency
+
+
+## Solution

7a8d2eb51dbf0d4ab6ebff4a7a6472d64a7ccb04
Abbey Masters <abbeykmasters@gmail.com>
2019-09-30 19:45:16 -0700

    first few passing

--- _tests_/stacks-and-queues.test.js
+++ _tests_/stacks-and-queues.test.js
@@ -1,24 +1,41 @@
-const stacksAndQueues = require('../challenges/stacksAndQueues/stacks-and-queues');
+const { Node, Stack, Queue} = require('../challenges/stacksAndQueues/stacks-and-queues');
 
 describe('Stacks and Queues', () => {
 
   it('Can successfully push onto a stack', () => {
-    const stack = new stacksAndQueues.Stack();
+    const stack = new Stack();
     stack.push('abbey');
     stack.push('masters');
-    expect(this.stack).toBe('masters');
+    expect(stack.top.value).toBe('masters');
   });
 
   it('Can successfully push multiple values onto a stack', () => {
-
+    const stack = new Stack();
+    stack.push('abbey');
+    stack.push('masters');
+    expect(stack.top.value).toBe('masters');
+    expect(stack.top.next.value).toBe('abbey');
   });
 
   it('Can successfully pop off the stack', () => {
-
+    const stack = new Stack();
+    stack.push('abbey');
+    stack.push('kathleen');
+    stack.push('masters');
+    const answer = stack.pop();
+    expect(answer).toBe('masters');
   });
 
   it('Can successfully empty a stack after multiple pops', () => {
-
+    const stack = new Stack();
+    stack.push('abbey');
+    stack.push('kathleen');
+    stack.push('masters');
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    const answer = stack.pop();
+    expect(answer).toBe(null);
   });
 
   it('Can successfully peek the next item on the stack', () => {
--- challenges/stacksAndQueues/stacks-and-queues.js
+++ challenges/stacksAndQueues/stacks-and-queues.js
@@ -1,24 +1,37 @@
 class Node {
-  constructor(next, value) {
-    this.next = next;
+  constructor(value) {
+    this.next = null;
     this.value = value;
   }
 }
 
 class Stack {
   constructor(value) {
-    this.stack = null;
+    this.top = null;
   }
 
+  /**
+   * Push a new node to the top of the stack
+   * @param value
+   * no return 
+   */
+
   push(element) {
-    let head = this.stack;
-    let newNode = new Node(null, element);
+    const node = new Node(element);
+    if (!this.top) this.top = node;
+    else {
+      node.next = this.top;
+      this.top = node;
+    }
   }
 
   pop() {
-    let head = this.stack;
-    this.stack = head.next;
-    return head.value;
+    let answer;
+    if (this.top) {
+      answer = this.top.value;
+      this.top = this.top.next;
+    } else answer = null;
+    return answer;
   }
 
   peek() {

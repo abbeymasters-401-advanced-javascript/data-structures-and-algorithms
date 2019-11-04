f89dcf3e33ba7697f609da76152e8bc65d7f7df4
Abbey Masters <abbeymasters@Abbeys-MacBook-Pro.local>
2019-11-03 17:20:52 -0800

    working on functions

--- challenges/hashTable/hashtable.js
+++ challenges/hashTable/hashtable.js
@@ -14,26 +14,40 @@ class HashTable {
   }
 
   add(key, value) {
+    const index = this.hash(key);
+    const bucket = this.arr[index];
+
+    if (bucket.length !== 0); // handle collisions
+    else bucket[index].push([key, value]);
 
   }
 
   get(key) {
     const index = this.hash(key);
     const bucket = this.arr[index];
-    if(bucket.length === 0) return null;
+
+    if (bucket.length === 0) return null;
+
     const item = bucket.find(([storedKey]) => storedKey === key);
-    if(!item) return null;
+    if (!item) return null;
     return item[1];
   }
 
   contains(key) {
+    // does key exist in table already?
+    const index = this.hash(key);
+    const bucket = this.arr[index];
 
+    if(bucket.get(key) !== null) return false;
+    else return true;
   }
 
   hash(key) {
     return this.hashAlgorithm(key);
   }
 }
+
+module.exports = HashTable;
 
 
 
